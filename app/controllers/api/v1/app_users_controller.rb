e012c6f9e49e02b77a1b053e0500196f56b71a3c
Ricardo Arrieta Zuñiga <ricardo727@hotmail.com>
2016-11-25 10:54:01 -0600

    Igual

--- app/controllers/api/v1/app_users_controller.rb
+++ app/controllers/api/v1/app_users_controller.rb
@@ -5,8 +5,6 @@ class Api::V1::AppUsersController < Api::V1::MasterApiController
 			raise "error"
 		else
 			@user = AppUser.from_omniauth(app_users_params)
-<<<<<<< HEAD
-=======
 			if @user.tokens.any?
 				if @user.tokens.last.is_valid?
 					@token = @user.tokens.create
@@ -16,25 +14,9 @@ class Api::V1::AppUsersController < Api::V1::MasterApiController
 			else
 				@token = @user.tokens.create
 			end
->>>>>>> 34854c68899985a9e68582cee0d9201d3ac60f1f
 			render "api/v1/app_users/show"
 		end
 	end
-
-<<<<<<< HEAD
-	
-
-# PATCH/PUT /users/1.json
-	def update
-	   if @user.update(user_params)
-	    respond_with({'status':'success'})
-	   else
-	    respond_with({'status':'error'})
-	   end
-	end
-
-=======
->>>>>>> 34854c68899985a9e68582cee0d9201d3ac60f1f
 	private
 
 	def app_users_params
--- spec/factories/tokens.rb
+++ spec/factories/tokens.rb
@@ -1,12 +1,6 @@
 FactoryGirl.define do
-  factory :token do
-<<<<<<< HEAD
-    expires_at "2016-11-23"
-    app_users_id 1
-    token "MyString"
-  end
-=======
-  	association :app_user, factory: :app_user
-    expires_at "2016-11-25 08:41:02"  end
->>>>>>> 34854c68899985a9e68582cee0d9201d3ac60f1f
+  	factory :token do
+  		association :app_user, factory: :app_user
+    	expires_at "2016-11-25 08:41:02"
+	end
 end
