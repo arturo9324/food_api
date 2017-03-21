3825587928344829faa76f6a8743f486eef46306
Arturo <arturo9324@gmail.com>
2017-03-21 17:05:28 -0600

    config puma

--- config/puma.rb
+++ config/puma.rb
@@ -13,9 +13,9 @@
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
-#workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-#threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5 )
-#threads threads_count, threads_count
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5 )
+threads threads_count, threads_count
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
@@ -26,23 +26,23 @@
 #
 # preload_app!
 
-#preload_app!
+preload_app!
 
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-threads threads_count, threads_count
+#threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+#threads threads_count, threads_count
 
-#rackup      DefaultRackup
-#port        ENV["PORT"] || 3000
-#environment ENV["RAILS_ENV"] || "development"
+rackup      DefaultRackup
+port        ENV["PORT"] || 3000
+environment ENV["RAILS_ENV"] || "development"
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
 
-port        ENV.fetch("PORT") { 3000 }
+#port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+#environment ENV.fetch("RAILS_ENV") { "development" }
 
 # The code in the `on_worker_boot` will be called if you are using
 # clustered mode by specifying a number of `workers`. After each worker
@@ -51,13 +51,13 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # or connections that may have been created at application boot, Ruby
 # cannot share connections between processes.
 #
-on_worker_boot do
-   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-end
-
 #on_worker_boot do
-#	ActiveRecord::Base.establish_connection
+#   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 #end
+
+on_worker_boot do
+	ActiveRecord::Base.establish_connection
+end
 
 # Allow puma to be restarted by `rails restart` command.
 # Specifies the number of `workers` to boot in clustered mode.
