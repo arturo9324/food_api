2fccc5d0073ee84e701a98573d6a14e3f60c08d1
Arturo <arturo9324@gmail.com>
2017-03-25 21:06:28 -0600

    mailer update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
      :address              => "smtp.gmail.com",
      :port                 => 587,
+     :domain               => 'https://food-api-app.herokuapp.com/:80',
      :user_name            => ENV['gmail_username'],
      :password             => ENV['gmail_password'],
      :authentication       => "plain",
