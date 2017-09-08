2304d5bc2e0c784d8a2a20cc8f8b85da23ae0c51
arturo9324 <arturo9324@gmail.com>
2017-09-08 12:46:16 -0500

    Update eaten_nutrient.rb
    Oprtional error

--- app/models/eaten_nutrient.rb
+++ app/models/eaten_nutrient.rb
@@ -1,7 +1,7 @@
 class EatenNutrient < ApplicationRecord
 	
 	belongs_to :nutrient, required: true
-	belongs_to :has_product
+	belongs_to :has_product, optional: true
 	validates_numericality_of :cantidad, presence: true, greater_than: 0.0 
 
 	scope :suma, -> { select("nutrient_id, sum(eaten_nutrients.cantidad) AS cantidad") }
